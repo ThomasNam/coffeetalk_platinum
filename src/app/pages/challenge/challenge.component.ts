89f73a1541101df7ba1840508ad03452dd7d5c50
red <purred@naver.com>
2025-02-10 13:22:23 +0900

    빌드 수정

--- src/app/pages/challenge/challenge.component.ts
+++ src/app/pages/challenge/challenge.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import {SearchFormComponent} from './search-form/search-form.component';
 
 @Component({
   selector: 'app-challenge',
-  imports: [],
+  imports: [SearchFormComponent],
   templateUrl: './challenge.component.html',
+  standalone: true,
   styleUrl: './challenge.component.scss'
 })
 export class ChallengeComponent {
---
+++ src/app/pages/challenge/search-form/search-form.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-search-form',
+  imports: [],
+  templateUrl: './search-form.component.html',
+  standalone: true,
+  styleUrl: './search-form.component.scss'
+})
+export class SearchFormComponent {
+
+}
